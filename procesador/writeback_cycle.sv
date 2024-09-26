69eb94b05633129b20443bae2bed6338c8d12080
sergio <sergiorc2000@hotmail.es>
2024-09-25 18:46:10 -0600

    Writeback cycle listo

--- procesador/writeback_cycle.sv
+++ procesador/writeback_cycle.sv
@@ -1,16 +1,19 @@
 module writeback_cycle(clk, rst, ResultSrcW, PCPlus4W, ALU_ResultW, ReadDataW, ResultW);
 
 	
-	input clk, rst, ResultSrcW;
-	input [31:0] PCPlus4W, ALU_ResultW, ReadDataW;
+	input clk, rst;
+	input [1:0]ResultSrcW;
+	input [31:0] PCPlus4W,
+	input [127:0]ALU_ResultW, ReadDataW;
 
-	output [31:0] ResultW;
+	output [127:0] ResultW;
 
 	
 	
-	mux2to1 result_mux (    
-						 .data0(ReadDataW),
-						 .data1(ALU_ResultW),
+	mux3to1 result_mux (    
+						 .data0(ALU_ResultW),
+						 .data1(ReadDataW),
+						 .data2({96'h0 ,PCPlus4W}),
 						 .sel(ResultSrcW),
 						 .dataOut(ResultW)
 						 );
